3da096e18d9e0359a0a470b610c4d57fcf7a47f8
McBrezel <ich8887@live.de>
2023-01-02 09:53:22 +0100

    Projektdateien hinzufügen.

---
+++ IPtoBinary/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace IPtoBinary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 30);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 30);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "0";
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(236, 30);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0";
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyUp);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(343, 30);
+            this.textBox4.MaxLength = 3;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "0";
+            this.textBox4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP-Address";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 80);
+            this.textBox5.MaxLength = 8;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(60, 20);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "00000000";
+            this.textBox5.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox5_KeyUp);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(113, 80);
+            this.textBox6.MaxLength = 8;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(60, 20);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "00000000";
+            this.textBox6.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox6_KeyUp);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(220, 80);
+            this.textBox7.MaxLength = 8;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(60, 20);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.Text = "00000000";
+            this.textBox7.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox7_KeyUp);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(327, 80);
+            this.textBox8.MaxLength = 8;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(60, 20);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.Text = "00000000";
+            this.textBox8.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox8_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Binary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(192, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(299, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 22);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ".";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 8;
+            this.trackBar1.Location = new System.Drawing.Point(6, 107);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(367, 45);
+            this.trackBar1.TabIndex = 13;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(369, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "/0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Network";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Broadcast";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(64, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "0.0.0.0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(64, 178);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(88, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "255.255.255.255";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(81, 80);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(15, 22);
+            this.label11.TabIndex = 19;
+            this.label11.Text = ".";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(192, 80);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(15, 22);
+            this.label12.TabIndex = 20;
+            this.label12.Text = ".";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(299, 80);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(15, 22);
+            this.label13.TabIndex = 21;
+            this.label13.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 204);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "IPtoBinary";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ IPtoBinary/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace IPtoBinary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //darkMode();
+        }
+
+        int dec;
+        int bin;
+
+        //IP boxes
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dec = Convert.ToInt16(textBox1.Text);
+                if (dec < 256) textBox5.Text = Convert.ToString(dec, 2).PadLeft(8, '0');
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dec = Convert.ToInt16(textBox2.Text);
+                if (dec < 256) textBox6.Text = Convert.ToString(dec, 2).PadLeft(8, '0');
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dec = Convert.ToInt16(textBox3.Text);
+                if (dec < 256) textBox7.Text = Convert.ToString(dec, 2).PadLeft(8, '0');
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox4_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dec = Convert.ToInt16(textBox4.Text);
+                if (dec < 256) textBox8.Text = Convert.ToString(dec, 2).PadLeft(8, '0');
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        //Binary boxes
+        private void textBox5_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                bin = Convert.ToInt16(textBox5.Text, 2);
+                textBox1.Text = Convert.ToString(bin, 10);
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox6_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                bin = Convert.ToInt16(textBox6.Text, 2);
+                textBox2.Text = Convert.ToString(bin, 10);
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox7_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                bin = Convert.ToInt16(textBox7.Text, 2);
+                textBox3.Text = Convert.ToString(bin, 10);
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        private void textBox8_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                bin = Convert.ToInt16(textBox8.Text, 2);
+                textBox4.Text = Convert.ToString(bin, 10);
+                updateAddrLabels();
+            }
+            catch { }
+        }
+
+        //subnet mask slider
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = "/" + trackBar1.Value.ToString();
+
+            updateAddrLabels();
+        }
+
+        //respond to changes
+        private void updateAddrLabels()
+        {
+            string addr = string.Concat(textBox5.Text.PadLeft(8, '0') + textBox6.Text.PadLeft(8, '0') + textBox7.Text.PadLeft(8, '0') + textBox8.Text.PadLeft(8, '0'));
+            char[] netaddr = addr.ToCharArray(), brdAddr = addr.ToCharArray();
+
+            if (addr.Length != 32) return;  //do nothing
+
+            for (int i = 31; i >= trackBar1.Value; i--)
+            {
+                netaddr[i] = (0).ToString()[0];
+                brdAddr[i] = (1).ToString()[0];
+            }
+
+            string netStr = new string(netaddr);
+            string brdStr = new string(brdAddr);
+
+            netStr = netStr.Insert(8, ".");
+            brdStr = brdStr.Insert(8, ".");
+            netStr = netStr.Insert(17, ".");
+            brdStr = brdStr.Insert(17, ".");
+            netStr = netStr.Insert(26, ".");
+            brdStr = brdStr.Insert(26, ".");
+
+            //octets
+            string[] nOct = netStr.Split('.');
+            string[] bOct = brdStr.Split('.');
+
+            try
+            {
+                netStr = Convert.ToString(Convert.ToByte(nOct[0], 2), 10) + "."
+                + Convert.ToString(Convert.ToByte(nOct[1], 2), 10) + "."
+                + Convert.ToString(Convert.ToByte(nOct[2], 2), 10) + "."
+                + Convert.ToString(Convert.ToByte(nOct[3], 2), 10);
+
+                brdStr = Convert.ToString(Convert.ToByte(bOct[0], 2), 10) + "."
+                        + Convert.ToString(Convert.ToByte(bOct[1], 2), 10) + "."
+                        + Convert.ToString(Convert.ToByte(bOct[2], 2), 10) + "."
+                        + Convert.ToString(Convert.ToByte(bOct[3], 2), 10);
+            }
+            catch { }
+
+            label9.Text = netStr;
+            label10.Text = brdStr;
+        }
+
+        public void darkMode()
+        {
+            Color darkgray = Color.FromArgb(45, 45, 48);
+            Color lightgray = Color.FromArgb(62, 62, 66);
+            Color textcolor = Color.FromArgb(241, 241, 241);
+
+            this.BackColor = darkgray;
+
+            foreach (Control ctl in Controls)
+            {
+                ctl.BackColor = lightgray;
+                ctl.ForeColor = textcolor;
+                //ctl.Font = new Font("Consolas", 8.25f, FontStyle.Regular);
+
+                if (ctl.GetType() == typeof(TextBox))
+                {
+                    ((TextBox)ctl).BorderStyle = BorderStyle.None;
+                }
+
+                else if (ctl.GetType() == typeof(Button))
+                {
+                    ((Button)ctl).FlatStyle = FlatStyle.Flat;
+                    ((Button)ctl).FlatAppearance.BorderSize = 0;
+                }
+
+                else
+                {
+                    ctl.BackColor = darkgray;
+                }
+            }
+        }
+    }
+}
